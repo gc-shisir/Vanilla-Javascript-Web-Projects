88c5f91ead28a23415e8ae625ca8e1a4a74613ab
Shisir GC <shisir.gc44@gmail.com>
2020-07-19 16:42:52 +0545

    background changer initial update

--- background changer/script.js
+++ background changer/script.js
@@ -7,11 +7,15 @@ function colorChange(){
   let color='';
   const colorArr=['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
   const arrLength=colorArr.length;
-  for(i=1,i<=6,i++){
-    let itemPosition=Math.floor(Math.random())
+  for(i=1;i<=6;i++){
+    let itemPosition=Math.floor(Math.random()*arrLength)
+    // console.log(itemPosition);
+    color+=colorArr[itemPosition];
+    // console.log(color);
   }
 
-  background.style.backgroundColor='#333';
+  background.style.backgroundColor='#'+color;
+  text.innerText = "Color: #"+color;
 }
 
 button.addEventListener('click',colorChange);
